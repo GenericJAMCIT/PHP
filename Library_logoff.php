b00dd5a20b53d1b8cdcc6dae7b1030634a03f8ee
GenericJAMCIT <55523541+GenericJAMCIT@users.noreply.github.com>
2020-06-09 21:47:59 -0700

    Create Library_logoff.php

---
+++ Library_logoff.php
@@ -0,0 +1,8 @@
+<?php 
+            session_name('Library_login');         
+            session_start();
+            session_destroy();                  
+?>
+<html>
+<h1>Thanks for visiting the club web site!</h1>      
+</html>
